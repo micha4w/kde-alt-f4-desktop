cfdfd5ea86d870381542e41dbfde586d56f3b7a7
benbc7 <b.cutler724@gmail.com>
2024-03-18 15:22:36 -0400

    Changed prompt to default to ShutDown instead of Logout

--- contents/code/main.js
+++ contents/code/main.js
@@ -1,6 +1,6 @@
 registerShortcut("Alt-F4-Desktop", "Close Window or show Logout Option", "Alt+F4", function() {
     if (workspace.activeWindow.desktopWindow) {
-        callDBus("org.kde.LogoutPrompt", "/LogoutPrompt", "", "promptLogout")
+        callDBus("org.kde.LogoutPrompt", "/LogoutPrompt", "", "promptShutDown")
     } else {
         workspace.activeWindow.closeWindow()
     }
