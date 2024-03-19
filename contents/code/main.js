182a325a320a1e023b1f119e66a8fff378ad5181
micha4w <85169193+micha4w@users.noreply.github.com>
2024-03-19 09:06:40 +0100

    fix: catch activeWindow being null

--- contents/code/main.js
+++ contents/code/main.js
@@ -1,5 +1,5 @@
 registerShortcut("Alt-F4-Desktop", "Close Window or show Logout Option", "Alt+F4", function() {
-    if (workspace.activeWindow.desktopWindow) {
+    if (workspace.activeWindow == null || workspace.activeWindow.desktopWindow) {
         callDBus("org.kde.LogoutPrompt", "/LogoutPrompt", "", "promptShutDown")
     } else {
         workspace.activeWindow.closeWindow()
