3e4629e0fca7bfb3bcdb795fed341b446120c283
micha4w <85169193+micha4w@users.noreply.github.com>
2024-06-18 20:45:37 +0200

    feat: show all options in plasma 6.1

--- contents/code/main.js
+++ contents/code/main.js
@@ -1,6 +1,6 @@
 registerShortcut("Alt-F4-Desktop", "Close Window or show Logout Option", "Alt+F4", function() {
     if (workspace.activeWindow == null || workspace.activeWindow.desktopWindow) {
-        callDBus("org.kde.LogoutPrompt", "/LogoutPrompt", "", "promptShutDown")
+        callDBus("org.kde.LogoutPrompt", "/LogoutPrompt", "", "promptAll")
     } else {
         workspace.activeWindow.closeWindow()
     }
