20d60d74aa727b29a8ef0e868cafcdfb96d0e184
Mr. Snow <tiwariutkarsh2705@gmail.com>
2026-07-11 20:28:11 +0530

    feat: improve Alt+F4 logout workflow

--- contents/code/main.js
+++ contents/code/main.js
@@ -1,9 +1,73 @@
-registerShortcut("Alt-F4-Desktop", "Close Window or show Logout Option", "Alt+F4", function() {
-    if (workspace.activeWindow == null || workspace.activeWindow.desktopWindow) {
-        callDBus("org.kde.LogoutPrompt", "/LogoutPrompt", "", "promptAll")
-    } else {
-        workspace.activeWindow.closeWindow()
+var waitingForLastWindow = false;
+
+function hasNormalWindows() {
+    var windows = workspace.windowList();
+
+    for (var i = 0; i < windows.length; ++i) {
+        var w = windows[i];
+
+        if (!w)
+            continue;
+
+        if (w.desktopWindow)
+            continue;
+
+        if (w.dock)
+            continue;
+
+        if (w.splash)
+            continue;
+
+        if (w.popupWindow)
+            continue;
+
+        if (w.notification)
+            continue;
+
+        if (w.utility)
+            continue;
+
+        return true;
     }
-})
 
+    return false;
+}
 
+workspace.windowRemoved.connect(function() {
+    if (!waitingForLastWindow)
+        return;
+
+    if (!hasNormalWindows()) {
+        waitingForLastWindow = false;
+
+        callDBus(
+            "org.kde.LogoutPrompt",
+            "/LogoutPrompt",
+            "",
+            "promptAll"
+        );
+    }
+});
+
+registerShortcut(
+    "Alt-F4-Desktop",
+    "Close Window or show Logout Option",
+    "Alt+F4",
+    function() {
+
+        var w = workspace.activeWindow;
+
+        if (!w || w.desktopWindow) {
+            callDBus(
+                "org.kde.LogoutPrompt",
+                "/LogoutPrompt",
+                "",
+                "promptAll"
+            );
+            return;
+        }
+
+        waitingForLastWindow = true;
+        w.closeWindow();
+    }
+);
