0e02d395075a0a9f06b18e2cb81fbf64149eae25
micha4w <micha4w@gmail.com>
2026-07-22 22:17:46 +0200

    fix: ignore xwaylandvideobridge

--- contents/code/main.js
+++ contents/code/main.js
@@ -1,5 +1,7 @@
 registerShortcut("Alt-F4-Desktop", "Close Window or show Logout Option", "Alt+F4", function() {
-    if (workspace.activeWindow == null || workspace.activeWindow.desktopWindow) {
+    if (workspace.activeWindow == null
+        || workspace.activeWindow.desktopWindow
+        || workspace.activeWindow.resourceClass == 'xwaylandvideobridge') {
         callDBus("org.kde.LogoutPrompt", "/LogoutPrompt", "", "promptAll")
     } else {
         workspace.activeWindow.closeWindow()
